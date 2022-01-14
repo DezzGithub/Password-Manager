c6eac99d4bf600facad5bd3ec4cc73fb421b37db
DezzGithub <97635359+DezzGithub@users.noreply.github.com>
2022-01-14 22:54:25 +0100

    Button
    Funktion hinzugefügt Username, Password, Website

--- Password Manger Main File.py
+++ Password Manger Main File.py
@@ -1,8 +1,9 @@
 
-   
 import sqlite3, hashlib
 from sqlite3.dbapi2 import Cursor
 from tkinter import *
+from tkinter import simpledialog
+from functools import partial
 
 #Database Code
 with sqlite3.connect("password_vault.db") as db:
@@ -13,6 +14,20 @@ CREATE TABLE IF NOT EXISTS masterpassword(
 id INTEGER PRIMARY KEY,
 password TEXT NOT NULL);
 """)
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS vault(
+id INTEGER PRIMARY KEY,
+website TEXT NOT NULL,
+username TEXT NOT NULL,
+password TEXT NOT NULL);
+""")
+
+#Create PopUp
+def PopUp(text):
+    answer = simpledialog.askstring("input string", text)
+
+    return answer
 
 #Initiate Window
 window = Tk()
@@ -100,13 +115,46 @@ def loginflaeche():
 def passwordVault():
     for widget in window.winfo_children():
         widget.destroy()
+
+        def addEntry():
+            text1= "Website"
+            text2= "Benutzername"
+            text3= "Passwort"
+
+            Website = PopUp(text1)
+            Benutzername = PopUp(text2)
+            Passwort = PopUp(text3)
+
+            insert_fields = """INSERT INTO vault(website,benutzername,passwort)
+            VALUES (?, ?, ?)"""
+
+            cursor.execute(insert_fields, (Website, Benutzername, Passwort))
+            db.commit() 
+
+            passwordVault()
+
+        def removeEntry(input):
+            cursor.execute("DELTE FROM vault WHERE id = ?", (input,))
+            db.commit()
+
+            passwordVault()
+
+
     window.geometry("700x350")
 
     lbl = Label(window, text="Passwort Verwahrung")
-    lbl.config(anchor=CENTER)
-    lbl.pack()
+    lbl.grid(column=1)
 
 
+    btn = Button(window, text="+", command=addEntry)
+    btn.grid(column=1, pady=10)
+
+    lbl = Label(window, text="Website")
+    lbl.grid(row=2, column=0, padx=80)
+    lbl = Label(window, text="Benutzername")
+    lbl.grid(row=2, column=1, padx=80)
+    lbl = Label(window, text="Passwort")
+    lbl.grid(row=2, column=2, padx=80)
 
 
 cursor.execute("SELECT * FROM masterpassword")
